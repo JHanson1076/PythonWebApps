449d3c88c2b5ece957f77969cdd5b6d0b26e96da
Jacob Hanson <Hans1076@bears.unco.edu>
2022-09-16 23:31:22 -0600

    Project 4 changes

--- Superhero/config/urls.py
+++ Superhero/config/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from hero.views import HulkView, Black_WidowView
+from hero.views import HulkView, Black_WidowView, Iron_ManView, Ghost_RiderView, Spider_ManView
 
 urlpatterns = [
     path('',        HulkView.as_view()),
     path('black_widow',        Black_WidowView.as_view()),
+    path('iron_man', Iron_ManView.as_view()),
+    path('ghost_rider', Ghost_RiderView.as_view()),
+    path('spider_man', Spider_ManView.as_view())
 ]
--- Superhero/hero/views.py
+++ Superhero/hero/views.py
@@ -23,3 +23,41 @@ class Black_WidowView(TemplateView):
             'id': 'Natasha Ramanoff',
             'image': '/static/images/black_widow.jpg'
         }      
+
+
+
+class Iron_ManView(TemplateView):
+    template_name = 'hero.html'
+
+
+    def get_context_data(self, **kwargs):
+        return {
+            'title': 'Iron Man',
+            'id': 'Tony Stark',
+            'image': '/static/images/iron_man.jpg'
+        }   
+
+class Ghost_RiderView(TemplateView):
+    template_name = 'hero.html'
+
+
+    def get_context_data(self, **kwargs):
+        return {
+            'title': 'Ghost Rider',
+            'id': 'Johnny Blaze',
+            'image': '/static/images/Ghost_Rider.jpg'
+        }   
+
+class Spider_ManView(TemplateView):
+    template_name = 'hero.html'
+
+
+    def get_context_data(self, **kwargs):
+        return {
+            'title': 'Spider-Man',
+            'id': 'Peter Parker',
+            'image': '/static/images/spider_man.jpg'
+        }   
+
+
+   
