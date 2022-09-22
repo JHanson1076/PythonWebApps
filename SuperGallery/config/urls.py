6bc2e65103f8ba25a2079c97efd771ef47861e32
Jacob Hanson <Hans1076@bears.unco.edu>
2022-09-21 18:44:15 -0600

    simple changes

--- SuperGallery/config/urls.py
+++ SuperGallery/config/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from photos.views import PhotoListView, PhotoView
 
 urlpatterns = [
-    path('<str:name>', PhotoView.as_view()),
+    path('<str:name>', PhotoView.as_View()),
     path('', PhotoListView.as_view()),
 ]
--- SuperGallery/photos/views.py
+++ SuperGallery/photos/views.py
@@ -21,4 +21,6 @@ class PhotoListView(TemplateView):
          return dict(photos=photos)
 
 
+ 
+
 
