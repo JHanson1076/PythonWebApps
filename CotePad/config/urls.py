ed6efdde267718d15290746c4f00df5e8cee8941
Jacob Hanson <Hans1076@bears.unco.edu>
2022-10-07 22:31:27 -0600

    Fixed cote/Superhero Views for project 5

--- CotePad/config/urls.py
+++ CotePad/config/urls.py
@@ -1,7 +1,14 @@
-
 from django.contrib import admin
 from django.urls import path
 
+from cote.views import CoteDetailView, CoteListView
+
 urlpatterns = [
+
+    # Admin
     path('admin/', admin.site.urls),
+
+    # Note
+    path('', CoteListView.as_view()),
+    path('<int:pk>', CoteDetailView.as_view()),
 ]
--- CotePad/cote/views.py
+++ CotePad/cote/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+from .models import Cote
+
+
+class CoteListView(TemplateView):
+    template_name = 'cotes.html'
+
+    def get_context_data(self, **kwargs):
+        return {
+            'object_list': Cote.objects.all()
+        }
+
+
+class CoteDetailView(TemplateView):
+    template_name = 'cote.html'
+
+    def get_context_data(self, **kwargs):
+        return {
+            'cote': Cote.objects.get(pk=kwargs['pk'])
+        }
