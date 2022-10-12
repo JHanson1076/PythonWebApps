6024a3d0087c5d9eb780e351bf05db585d3f0ae7
Jacob Hanson <Hans1076@bears.unco.edu>
2022-10-12 14:44:33 -0600

    Please grade for Project 6 add hero Strength/Weak

--- CotePad/cote/models.py
+++ CotePad/cote/models.py
@@ -3,8 +3,10 @@ from django.db import models
 class Cote(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
+    strength = models.CharField(max_length=200,default='anything')
+    weak = models.CharField(max_length=200,default='anything')
     image = models.CharField(max_length=200)
 # Create your models here.
 
     def __str__(self):
-        return f'{self.pk}. {self.name} - {self.description}'
+        return f'{self.pk}. {self.name} - {self.description} - {self.strength} - {self.weak}'
